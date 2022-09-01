6685c6a53dec6cec05e091862108665442dcdcc1
priyanshuvij2201 <PRIYANSHUVIJ456@GMAIL.COM>
2022-09-01 14:06:30 +0530

    BASIC CSS + JS DONE
    Console log error not updated

---
+++ script.js
@@ -0,0 +1,18 @@
+const intialPrice=document.querySelector("#initial-price");
+// const currentPrice=document.querySelector("#current-price");
+// const quantity=document.querySelector("#quantity"); 
+const checkButton=document.querySelector("#profit-loss");
+// const outputBoxArea=document.querySelector("#output-boxarea");
+// const ab=currentPrice.value;
+// console.log(ab);
+// console.log(intialPrice.value);
+console.log("Hello woRLD");
+
+// function profit(){
+//     if(currentPrice>intialPrice)
+//     {const loss=currentPrice.value-intialPrice.value;}
+//     console.log(loss);
+// }
+
+
+checkButton.addEventListener('click',console.log(intialPrice.value));
