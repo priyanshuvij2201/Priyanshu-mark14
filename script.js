11abd1550d9d321a6d69866af68548871d60631f
priyanshuvij2201 <PRIYANSHUVIJ456@GMAIL.COM>
2022-09-05 00:03:41 +0530

    completed mark 14

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
 const intialPrice=document.querySelector("#initial-price");
-// const currentPrice=document.querySelector("#current-price");
-// const quantity=document.querySelector("#quantity"); 
+const currentPrice=document.querySelector("#current-price");
+const quantity=document.querySelector("#quantity"); 
 const checkButton=document.querySelector("#profit-loss");
-// const outputBoxArea=document.querySelector("#output-boxarea");
+const outputBoxArea=document.querySelector("#output-boxarea");
 // const ab=currentPrice.value;
 // console.log(ab);
-// console.log(intialPrice.value);
-console.log("Hello woRLD");
 
-// function profit(){
-//     if(currentPrice>intialPrice)
-//     {const loss=currentPrice.value-intialPrice.value;}
-//     console.log(loss);
-// }
+function calculateProfitandLoss(){
+    var profitorloss=0;
+    if((intialPrice.value && currentPrice.value &&quantity.value)<=0)
+    {
+        printmessage("Please fill out all fields");
+    }
+    else
+    {
+    var bp=intialPrice.value;
+    var sp=currentPrice.value;
+    var profitloss=0;
+    var quant=quantity.value;
+
+    if(currentPrice.value>intialPrice.value){
+    profitloss=(sp-bp)*quant;
+    profitpercent=(profitloss/(bp*quant))*100;
+    profitorloss=1;
+    
+    printmessage('The profit is '+ Number.parseFloat(profitloss).toFixed(2) + " and the profit percentage is "  + Number.parseFloat(profitpercent).toFixed(2) + "%");
+}
+    else{
+    profitloss=(bp-sp)*quant;
+    profitpercent=(profitloss/(bp*quant))*100;
+    profitorloss=0;
+    printmessage('The loss is '+ Number.parseFloat(profitloss).toFixed(2) + " and the loss percentage is "  + Number.parseFloat(profitpercent).toFixed(2) + "%");
+}
+
+    console.log(profitloss,profitpercent); 
+    return profitorloss;
+    }
+    function printmessage(message)
+    {
+        if(profitorloss==1)
+      {  outputBoxArea.style.color='green';
+        outputBoxArea.innerHTML=message;}
+        else{
+            outputBoxArea.style.color='red';
+            outputBoxArea.innerHTML=message;
+        }
+
+    }
+}
 
 
-checkButton.addEventListener('click',console.log(intialPrice.value));
+checkButton.addEventListener('click',calculateProfitandLoss);
